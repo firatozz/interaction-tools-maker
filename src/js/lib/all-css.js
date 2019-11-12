35d64f8b8ade54345cacf4622a79fb9e1fd2923b
firatozz <firatoz01@gmail.com>
2019-11-12 23:13:21 +0300

    update-general-tab

--- src/js/lib/all-css.js
+++ src/js/lib/all-css.js
@@ -7,13 +7,13 @@
  function addCSS() {
 
      var css = document.createTextNode(
-         ".swFuncPopup_bgEl{display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: #000; opacity: 0.4; z-index: 10001;}" +
-         ".swFunctionalPopup{display: block; position: fixed; width: " + width + "px; height: " + height + "px; z-index: 10002;" +
+         ".itmBgEl{display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: #000; opacity: 0.4; z-index: 10001;}" +
+         ".itmPopupEl{display: block; position: fixed; width: " + width + "px; height: " + height + "px; z-index: 10002;" +
          constAnimation[preAnimation][0][0] +
          "transition:.5s ease-in-out;visibility: hidden;transform: translateX(calc(50% - " + width / 2 + "px)) translateY(calc(50% - " + height / 2 + "px)); left:calc(50% - " + width / 2 + "px); top:calc(50% - " + height / 2 + "px);box-shadow: 0px 0px 5px 0 rgba(0,0,0,0.5); z-index: 10002;}" +
-         ".swFunctionalPopup.active{" + constAnimation[preAnimation][1][0] + "; visibility: visible;}" +
-         ".swImg{width:" + width + "px; height: " + height + "px;}" +
-         ".swCloseEl{position: absolute;display:inline-block;width: " + height / 50 + "%;height:" + width / 50 + "%; top: 2%;right: 4%;cursor: pointer; border-radius: 50%;}" +
+         ".itmPopupEl.active{" + constAnimation[preAnimation][1][0] + "; visibility: visible;}" +
+         ".itmImage{width:" + width + "px; height: " + height + "px;}" +
+         ".itmCloseEl{position: absolute;display:inline-block;width: " + height / 50 + "%;height:" + width / 50 + "%; top: 2%;right: 4%;cursor: pointer; border-radius: 50%;}" +
          this.css
      );
 
--- src/js/lib/general-tab.js
+++ src/js/lib/general-tab.js
@@ -5,16 +5,34 @@ var config = allConfig();
 var event = addEvent();
 var bgEl = config.itmBgEl;
 var closeEl = config.itmCloseBtnEl;
+var popupEl = config.itmPopupEl;
 var autoClose = config.generalAutoClose;
 var autoCloseDelay = config.generalAutoCloseDelay;
+
+
+function hidePopUp() {
+
+    bgEl.style.display = "none";
+    popupEl.className = "itmPopupEl";
+
+    // Set body overflow back to default to show scrollbars
+    document.body.style.overflow = "auto";
+}
+
+
+//For Dismiss func
+function clearBody() {
+    bgEl.remove();
+    popupEl.remove();
+}
+
 
 function dismissOnOverlayClick() {
     // Handle the popup click overlay
     event(bgEl, "click", () => {
-
-        this.hidePopUp(); /* ATTENTION */
+        hidePopUp(); /* ATTENTION */
         setTimeout(() => {
-            this.clearBody() /* ATTENTION */
+            clearBody() /* ATTENTION */
         }, 1000);
     });
 }
@@ -22,7 +40,7 @@ function dismissOnOverlayClick() {
 function dismissOnContentClick() {
     // If targetOpenNewTab have worked, the box will close
     event(closeEl, "click", () => {
-        this.clearBody(); /* ATTENTION */
+        clearBody(); /* ATTENTION */
         document.body.style.overflow = "visible";
     });
 }
@@ -30,9 +48,9 @@ function dismissOnContentClick() {
 function dismissOnCloseBtnClick() {
     // Handle the popup close button
     event(this.closeBtnEl, "click", () => {
-        this.hidePopUp(); /* ATTENTION */
+        hidePopUp(); /* ATTENTION */
         setTimeout(() => {
-            this.clearBody() /* ATTENTION */
+            clearBody() /* ATTENTION */
         }, 1000);
     });
 }
@@ -41,9 +59,9 @@ function dismissAutoClose() {
     event(bgEl, "mousemove", () => {
         if (autoClose) {
             setTimeout(() => {
-                this.hidePopUp(); /* ATTENTION */
+                hidePopUp(); /* ATTENTION */
                 setTimeout(() => {
-                    this.clearBody() /* ATTENTION */
+                    clearBody() /* ATTENTION */
                 }, 800);
             }, autoCloseDelay * 1000);
         }
